9ea830648e7ccbb25e05c17cd98aa447922c29d6
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-11 11:53:20 -0600

    argument handling for client.py

---
+++ battleShip/client.py
@@ -0,0 +1,11 @@
+import sys
+
+ipAdd = sys.argv[1] #IP Address
+port  = sys.argv[2] #port number
+x     = sys.argv[3] #x coordinate
+y     = sys.argv[4] #y coordinate
+
+print "This is the IP Address", ipAdd
+print "This is the port number", port
+print "This is the x coordinate", x
+print "This is the y coordinate", y
--- battleShip/server.py
+++ battleShip/server.py
@@ -1,10 +1,14 @@
 import sys
-portNum = sys.argv[1]
-fileName = sys.argv[2]
+
+portNum  = sys.argv[1] #port number
+fileName = sys.argv[2] #file name for game board
+
 file = open(fileName,"r")
+
 print "This is the port number", portNum
 print "This is the game board"
-for line in file:
+
+for line in file: #print each line of the board
     print line
 file.close()
 print "End of script."
