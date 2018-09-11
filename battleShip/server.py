2f311067c2df924009ff4e64acb2a480c7f12ab8
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-11 09:26:35 -0600

    argument handling for server.py

--- battleShip/server.py
+++ battleShip/server.py
@@ -1,1 +1,10 @@
-print "hi"
+import sys
+portNum = sys.argv[1]
+fileName = sys.argv[2]
+file = open(fileName,"r")
+print "This is the port number", portNum
+print "This is the game board"
+for line in file:
+    print line
+file.close()
+print "End of script."
