305ce84334bde6dc19f4744d37f9f42c4ee6555d
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-12 10:32:35 -0600

    added socket handling on server.py

--- battleShip/server.py
+++ battleShip/server.py
@@ -1,4 +1,6 @@
 import sys
+import httplib
+import socket
 
 portNum  = sys.argv[1] #port number
 fileName = sys.argv[2] #file name for game board
@@ -6,9 +8,14 @@ fileName = sys.argv[2] #file name for game board
 file = open(fileName,"r")
 
 print "This is the port number", portNum
-print "This is the game board"
-
+print "This is the game board:"
 for line in file: #print each line of the board
     print line
 file.close()
+
+#socket handling
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 1025))
+s.listen(1)
+
 print "End of script."
