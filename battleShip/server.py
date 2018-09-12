1bd07600621cc3a933e99e1d6ba72337ba9af069
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-12 10:46:26 -0600

    updated server.py

--- battleShip/server.py
+++ battleShip/server.py
@@ -2,8 +2,16 @@ import sys
 import httplib
 import socket
 
-portNum  = sys.argv[1] #port number
+portNum = sys.argv[1] #port number
+portNum = int(portNum) #convert port number to an integer
 fileName = sys.argv[2] #file name for game board
+
+
+#socket handling
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', portNum))
+s.listen(1)
+
 
 file = open(fileName,"r")
 
@@ -13,9 +21,5 @@ for line in file: #print each line of the board
     print line
 file.close()
 
-#socket handling
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 1025))
-s.listen(1)
-
+s.close()
 print "End of script."
