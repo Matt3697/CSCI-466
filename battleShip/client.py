09c655307f3e6abe85004c8af2d2dcfbe0bdc0aa
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-12 11:24:14 -0600

    Established connection on local machine from client to server

--- battleShip/client.py
+++ battleShip/client.py
@@ -1,11 +1,23 @@
 import sys
+import socket
+import httplib
 
-ipAdd = sys.argv[1] #IP Address
-port  = sys.argv[2] #port number
-x     = sys.argv[3] #x coordinate
-y     = sys.argv[4] #y coordinate
+#ipAdd    = sys.argv[1] #IP Address
+portNum  = sys.argv[1] #port number
+portNum  = int(portNum)
+#x        = sys.argv[3] #x coordinate
+#y        = sys.argv[4] #y coordinate
 
-print "This is the IP Address", ipAdd
-print "This is the port number", port
-print "This is the x coordinate", x
-print "This is the y coordinate", y
+#print "This is the IP Address:", ipAdd
+print "This is the port number:", portNum
+#print "This is the x coordinate:", x
+#print "This is the y coordinate:", y
+
+host = socket.gethostname()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, portNum))
+s.send(bytes('Hello'))
+#data = conn.recv(1024)
+s.close()
--- battleShip/server.py
+++ battleShip/server.py
@@ -5,21 +5,33 @@ import socket
 portNum = sys.argv[1] #port number
 portNum = int(portNum) #convert port number to an integer
 fileName = sys.argv[2] #file name for game board
+host = socket.gethostname()
 
 
 #socket handling
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', portNum))
+s.bind((host, portNum))
 s.listen(1)
+
+
+conn, addr = s.accept()
+#data = conn.recv(BUFFER_SIZE)
+#conn.send(data)
+print('Got connection from ', addr[0], '(', addr[1], ')')
+print(conn.recv(1024))
+conn.close()
 
 
 file = open(fileName,"r")
 
-print "This is the port number", portNum
-print "This is the game board:"
-for line in file: #print each line of the board
-    print line
+#print "This is the port number", portNum
+#print "This is the game board:"
+#for line in file: #print each line of the board
+#    print line
 file.close()
+
+
+
 
 s.close()
 print "End of script."
