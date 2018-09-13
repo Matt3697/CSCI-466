1a69972e1ab36e84d8c3e7c35ce005912d007068
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-13 13:28:34 -0600

    added some helper functions

--- battleShip/server.py
+++ battleShip/server.py
@@ -3,45 +3,45 @@ import httplib
 import socket
 from http.server import HTTPServer
 
-portNum = sys.argv[1] #port number
-portNum = int(portNum) #convert port number to an integer
+portNum  = sys.argv[1] #port number
+portNum  = int(portNum) #convert port number to an integer
 fileName = sys.argv[2] #file name for game board
-host = socket.gethostname()
+host     = socket.gethostname()
+
 
 class handleRequests():
     def on_POST():
         print("made it")
 
-#socket handling
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, portNum))
-serv_address = (host, portNum)
-s.listen(1)
+def throw_argument_error()
+    print ("Error: incorrect arguments.")
 
 def handle_args()
-    if()
+    if(len(sys.argv) != 3)
+        throw_argument_error()
+
+def socket_handling()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, portNum))
+    serv_address = (host, portNum)
+    s.listen(1)
+
 def start_server()
     httpd = HTTPServer(serv_address, handleRequests)
     httpd.serve_forever()
 
-
-conn, addr = s.accept()
+#conn, addr = s.accept()
 #data = conn.recv(BUFFER_SIZE)
 #conn.send(data)
-print('Got connection from ', addr[0], '(', addr[1], ')')
-print(conn.recv(1024))
+#print('Got connection from ', addr[0], '(', addr[1], ')')
+#print(conn.recv(1024))
+#file = open(fileName,"r")
+#file.close()
 
-
-
-file = open(fileName,"r")
-file.close()
-
-
-
-conn.close()
-s.close()
 
 def main()
     handle_args() #make sure arguments are valid
     start_server()
+    conn.close()
+    s.close()
     print "End of script."
