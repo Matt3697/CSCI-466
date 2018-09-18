6a32c6e46b0defdfd65c37fcabb747c61d9c675c
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-09-18 15:09:14 -0600

    server mod

--- battleShip/server.py
+++ battleShip/server.py
@@ -9,9 +9,26 @@
 import sys
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
+
 arg_length = len(sys.argv)
 portNum    = 0
 
+class HandleRequests(BaseHTTPRequestHandler):
+    def do_POST(self):
+        coordinates = self.get_coordinates()
+        self.send_response(200)
+        self.end_headers()
+        # need to check here if the spot on the board gets hit
+
+    def do_GET(self):
+        self.send_response(404)
+        self.end_headers()
+
+    def get_coordinates(self):
+        raw_data = self.rfile.read(int(self.headers['Content-Length']))
+        byte_coordinates = dict(parse_qsl(raw_data))
+        string_coordinates = {key.decode(): val.decode() for key, val in byte_coordinates.items()} #convert from bytes to str
+        return string_coordinates
 
 def throw_argument_error():
     print ("Error: incorrect arguments. Try python3 server.py <port_number> <file_name>")
