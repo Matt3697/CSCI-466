2b1f87c3fa3d51288d30fa4471a029233452d080
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-09-19 10:54:36 -0600

    established a connection

--- battleShip/client.py
+++ battleShip/client.py
@@ -6,7 +6,6 @@
 #by an opponent in Battleship.
 #
 import sys
-#import http.client
 import requests
 
 ipAdd      = sys.argv[1]  #IP Address
@@ -15,7 +14,7 @@ x          = sys.argv[3]  #x coordinate
 y          = sys.argv[4]  #y coordinate
 
 def throw_argument_error():
-    print ("Error: incorrect arguments. Try python3 server.py <ip_address> <port_number> <xCoordinate> <yCoordinate>")
+    print ("Error: incorrect arguments. Try python server.py <ip_address> <port_number> <xCoordinate> <yCoordinate>")
     sys.exit(0)
 
 def handle_args():
@@ -24,21 +23,13 @@ def handle_args():
 
 def client_connection():
     try:
-        #payload = {'key1': 'value1', 'key2': 'value2'}
         newAddress = 'http://' + ipAdd + ':' + portNum #+ '?x=' + x + '&y=' + y #concatonate ip address w/ the port number and x/y coordinates
         print("Connecting to " + newAddress)
         payload = {'x':x, 'y':y}
         r = requests.post(newAddress, data=payload)
         r.status_code
         print(r.text)
-        #connection = http.client.HTTPConnection(ipAdd) #get conneciton to other system
-        #connection.connect()
-        #connection.request("POST", newAddress, params, headers={})
-        #response = connection.getresponse()    #get the response
-        #print(response.status, response.reason)
-        #r = response.read()
-        #print (r)
-        #connection.close()
+        
     except Exception as e:
         print(str(e))
 
--- battleShip/server.py
+++ battleShip/server.py
@@ -9,28 +9,28 @@
 import sys
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
-from http.server import CGIHTTPRequestHandler
 
 class HandleRequests(BaseHTTPRequestHandler):
     def do_POST(self):
-        coordinates = self.get_coordinates()
+        #coordinates = self.get_coordinates()
         print("yo")
         self.send_response(200)
         self.end_headers()
         # check here if the spot on the board gets hit
 
-    def do_GET(self):
-        self.send_response(404)
-        self.end_headers()
+    #def do_GET(self):
+        #self.send_response(404)
+        #self.end_headers()
 
-    def get_coordinates(self):
-        raw_data = self.rfile.read(int(self.headers['Content-Length']))
-        byte_coordinates = dict(parse_qsl(raw_data))
-        string_coordinates = {key.decode(): val.decode() for key, val in byte_coordinates.items()} #convert from bytes to str
-        return string_coordinates
+    #def get_coordinates(self):
+        #raw_data = self.rfile.read(int(self.headers['Content-Length']))
+        #byte_coordinates = dict(parse_qsl(raw_data))
+        #string_coordinates = {key.decode(): val.decode() for key, val in byte_coordinates.items()} #convert from bytes to str
+        #return string_coordinates
 
 arg_length = len(sys.argv)
-portNum    = 0
+port    = sys.argv[1]
+portNum = int(port)
 
 def throw_argument_error():
     print ("Error: incorrect arguments. Try python3 server.py <port_number> <file_name>")
@@ -39,14 +39,11 @@ def throw_argument_error():
 def handle_args():
     if (arg_length != 3):
         throw_argument_error()
-    else:
-        portNum    = sys.argv[1]  #port number
-        #portNum    = int(portNum) #convert port number to an integer
-        fileName   = sys.argv[2]  #file name for game board
 
 def start_server():
     try:
         server_address = ('127.0.0.1', portNum)
+        print("Connecting to 127.0.0.1 on port " + port)
         httpd = HTTPServer(server_address, HandleRequests)
         httpd.serve_forever()
     except Exception as e:
