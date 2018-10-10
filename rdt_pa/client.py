2afc6fc0fc1a03f8806dd65c9994a94fe1037549
Dillon Tice <dtice@localhost.localdomain>
2018-10-09 18:13:07 -0600

    commenting and reformatting

--- rdt_pa/client.py
+++ rdt_pa/client.py
@@ -3,39 +3,49 @@ import RDT
 import time
 
 if __name__ == '__main__':
-    parser =  argparse.ArgumentParser(description='Quotation client talking to a Pig Latin server.')
+    parser = argparse.ArgumentParser(description='Quotation client talking to a Pig Latin server.')
     parser.add_argument('server', help='Server.')
     parser.add_argument('port', help='Port.', type=int)
     args = parser.parse_args()
-    
+
     msg_L = ['The use of COBOL cripples the mind; its teaching should, therefore, be regarded as a criminal offense. -- Edsgar Dijkstra',
             'C makes it easy to shoot yourself in the foot; C++ makes it harder, but when you do, it blows away your whole leg. -- Bjarne Stroustrup',
             'A mathematician is a device for turning coffee into theorems. -- Paul Erdos',
             'Grove giveth and Gates taketh away. -- Bob Metcalfe (inventor of Ethernet) on the trend of hardware speedups not being able to keep up with software demands',
             'Wise men make proverbs, but fools repeat them. -- Samuel Palmer (1805-80)']
-    
-     
-    timeout = 2 #send the next message if no response
+
+    # send the next message if no response
+    timeout = 2
     time_of_last_data = time.time()
-     
+
     rdt = RDT.RDT('client', args.server, args.port)
     for msg_S in msg_L:
-        print('Converting: '+msg_S)
+        print('Converting using RDT1.0: '+msg_S)
         rdt.rdt_1_0_send(msg_S)
-       
-        # try to receive message before timeout 
+
+        # TODO Implement other RDT .send()
+        # print('Converting using RDT2.1: '+msg_S)
+        # rdt.rdt_2_1_send(msg_S)
+        # print('Converting using RDT3.0: '+msg_S)
+        # rdt.rdt_3_0_send(msg_S)
+        # try to receive message before timeout
+
         msg_S = None
-        while msg_S == None:
+        while msg_S is None:
             msg_S = rdt.rdt_1_0_receive()
+
+            # TODO implement other RDT .receive()
+            # msg_S = rdt.rdt_2_1_receive()
+            # msg_S = rdt.rdt_3_0_receive()
+            
             if msg_S is None:
                 if time_of_last_data + timeout < time.time():
                     break
                 else:
                     continue
         time_of_last_data = time.time()
-        
-        #print the result
+
+        # print the result
         if msg_S:
             print('to: '+msg_S+'\n')
-        
     rdt.disconnect()
--- rdt_pa/server.py
+++ rdt_pa/server.py
@@ -4,18 +4,19 @@ import time
 
 
 def makePigLatin(word):
-    m  = len(word)
-    vowels = "a", "e", "i", "o", "u", "y" 
-    if m<3 or word=="the":
+    m = len(word)
+    vowels = "a", "e", "i", "o", "u", "y"
+    if m < 3 or word == "the":
         return word
     else:
         for i in vowels:
             if word.find(i) < m and word.find(i) != -1:
                 m = word.find(i)
-        if m==0:
-            return word+"way" 
+        if m == 0:
+            return word+"way"
         else:
-            return word[m:]+word[:m]+"ay" 
+            return word[m:]+word[:m]+"ay"
+
 
 def piglatinize(message):
     essagemay = ""
@@ -26,16 +27,17 @@ def piglatinize(message):
 
 
 if __name__ == '__main__':
-    parser =  argparse.ArgumentParser(description='Pig Latin conversion server.')
+    parser = argparse.ArgumentParser(description='Pig Latin conversion server.')
     parser.add_argument('port', help='Port.', type=int)
     args = parser.parse_args()
-    
-    timeout = 5 #close connection if no new data within 5 seconds
+
+    # close connection if no new data within 5 seconds
+    timeout = 5
+
     time_of_last_data = time.time()
-    
     rdt = RDT.RDT('server', None, args.port)
     while(True):
-        #try to receiver message before timeout
+        # try to receiver message before timeout
         msg_S = rdt.rdt_1_0_receive()
         if msg_S is None:
             if time_of_last_data + timeout < time.time():
@@ -43,14 +45,10 @@ if __name__ == '__main__':
             else:
                 continue
         time_of_last_data = time.time()
-        
-        #convert and reply
+
+        # convert and reply
         rep_msg_S = piglatinize(msg_S)
         print('Converted %s \nto \n%s\n' % (msg_S, rep_msg_S))
         rdt.rdt_1_0_send(rep_msg_S)
-        
-    rdt.disconnect()
 
-    
-    
-    
+    rdt.disconnect()
